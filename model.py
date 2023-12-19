4414436a1dba5d37e8e44903d2da671c4a222d1c
Yash Prakash <yashprakash13@gmail.com>
2023-12-19 12:05:55 -0500

    added test images

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 from io import BytesIO
-from typing import Union
+from typing import List, Union
 
 from fastapi.responses import JSONResponse
 from deepface import DeepFace
 from fastapi import FastAPI
 import numpy as np
 from PIL import Image
+from pydantic import BaseModel
 from ray import serve
 import requests
 
@@ -22,26 +23,13 @@ class FaceDetector:
         self.logger = Logger(module="FaceDetector")
         self.logger.info("Initialized model.")
 
-    def _process_image(img_path: Union[np.ndarray, str]) -> np.ndarray:
-        if isinstance(img_path, str):
-            # If img_path is a string, assume it's a file path and download the image
-            response = requests.get(img_path)
-            img = Image.open(BytesIO(response.content))
-            img_np = np.array(img)
-            return img_np
-        elif isinstance(img_path, np.ndarray):
-            # If img_path is a NumPy array, use it directly
-            img_np = img_path
-            return img_np
-        else:
-            # Handle other cases or raise an error if needed
-            raise ValueError("Unsupported type for img_path. Must be str or np.ndarray.")
-
     @app.post("/detect-face")
-    def detect_face(self, img_path: Union[np.ndarray, str]) -> dict:
+    def detect_face(self, img_path: str) -> JSONResponse:
         # Run inference
-        img = self._process_image(img_path)
-        response = DeepFace.extract_faces(img_path=img, detector_backend="mtcnn")        
+        response = requests.get(img_path)
+        img = Image.open(BytesIO(response.content))
+        img_np = np.array(img)
+        response = DeepFace.extract_faces(img_path=img_np, detector_backend="mtcnn")        
         self.logger.info("Output done.")
         output = []
         for index, face in enumerate(response):
--- model_client.py
+++ model_client.py
@@ -19,12 +19,12 @@ image_np = np.array(image)
 
 # Convert the NumPy array to a base64-encoded string
 image_base64 = base64.b64encode(image_np.tobytes()).decode('utf-8')
-
+image_list = image_np.tolist()
 # Define the API endpoint
 api_endpoint = "http://localhost:8000/detect-face"
 
 # Prepare the payload
-payload = {"img_path": image_base64}
+payload = {"img_path": image_list}
 
 # Send the cURL request using the requests library
 response = requests.post(api_endpoint, json=payload)
